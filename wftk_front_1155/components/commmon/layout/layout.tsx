b253516a0a9ea1ade5a4d888640bb0bbaf6f2083
woohm402 <woohm404@gmail.com>
2022-02-06 01:26:30 +0900

    layout 생성

--- wftk_front_1155/components/commmon/layout/layout.tsx
+++ wftk_front_1155/components/commmon/layout/layout.tsx
@@ -1,21 +1,24 @@
 import { PropsWithChildren } from 'react';
-import styles from '../../../styles/Home.module.css';
+import homeStyles from '../../../styles/Home.module.css';
+import styles from './layout.module.scss';
 import Head from 'next/head';
 
 interface Props {}
 
 export const Layout = ({ children }: PropsWithChildren<Props>): JSX.Element => {
   return (
-    <div className={styles.container}>
+    <div className={homeStyles.container}>
       <Head>
         <title>WaffleStudio ERC-1155</title>
         <meta name="description" content="wafflestudio erc 1155 token web" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>{children}</main>
+      <header className={styles.header}>Waffle Token DApp</header>
 
-      <footer className={styles.footer} />
+      <main className={homeStyles.main}>{children}</main>
+
+      <footer className={homeStyles.footer} />
     </div>
   );
 };
