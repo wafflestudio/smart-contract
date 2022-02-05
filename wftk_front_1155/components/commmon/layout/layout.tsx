4cce8192af5c704b65f1710925ecae55609ab07e
woohm402 <woohm404@gmail.com>
2022-02-06 00:30:13 +0900

    create layout

---
+++ wftk_front_1155/components/commmon/layout/layout.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren } from "react";
+import styles from "../../../styles/Home.module.css";
+import Head from "next/head";
+
+interface Props {}
+
+const Layout = ({ children }: PropsWithChildren<Props>): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>WaffleStudio ERC-1155</title>
+        <meta name="description" content="wafflestudio erc 1155 token web" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>{children}</main>
+
+      <footer className={styles.footer} />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ wftk_front_1155/pages/waffle/[id].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Layout from "../../components/commmon/layout/layout";
+
+const Waffle: NextPage = () => {
+  return <Layout>home</Layout>;
+};
+
+export default Waffle;
