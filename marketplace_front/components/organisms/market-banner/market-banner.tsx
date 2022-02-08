87468aaef686fb0db0f9fd717841577da02e1048
GoGiants1 <chooi221@snu.ac.kr>
2022-02-09 00:00:07 +0900

    Add Market page

--- marketplace_front/components/organisms/market-banner/market-banner.tsx
+++ marketplace_front/components/organisms/market-banner/market-banner.tsx
@@ -1,9 +1,14 @@
 import classNames from 'classnames';
+import Link from 'next/link';
 
 interface Props {
   className?: string;
 }
 
 export const MarketBanner = ({ className }: Props) => {
-  return <section className={classNames(className)}>마켓 보러가기</section>;
+  return (
+    <section className={classNames(className)}>
+      <Link href={'/market'}>마켓 보러가기</Link>
+    </section>
+  );
 };
---
+++ marketplace_front/pages/market/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+
+import { Market } from '../../components/pages/market/market';
+const MarketPage: NextPage = () => {
+  return <Market />;
+};
+
+export default MarketPage;
