44fd5061640763a8296123146141514da29b2329
woohm402 <woohm404@gmail.com>
2022-02-09 00:02:21 +0900

    이용 내역 뷰 구현

---
+++ marketplace_front/components/templates/home-transfer-history/home-transfer-history.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+import { Typography } from '../../atoms/typography/typography';
+import { TinyList } from '../../organisms/tiny-list/tiny-list';
+
+import styles from './home-transfer-history.module.scss';
+
+export const HomeTransferHistory = () => {
+  return (
+    <section className={styles.wrapper}>
+      <header className={styles.header}>
+        <Typography as="h3">이용내역</Typography>
+        <Link href="/">{'자세히 보기 ->'}</Link>
+      </header>
+      <TinyList
+        dataSource={[
+          { label: 'Lorem', caption: '1분 전' },
+          { label: 'Lorem', caption: '10분 전' },
+          { label: 'Ipsum', caption: '1억년 전' },
+          { label: 'Lorem', caption: '옛날이' },
+          { label: 'Ipsum', caption: '너무나' },
+          { label: 'Lorem', caption: '그리워' },
+          { label: 'Ipsum', caption: '보고픈' },
+          { label: 'Lorem', caption: '엄마찾아' },
+          { label: 'Ipsum', caption: '모두함께' },
+          { label: 'Lorem', caption: '나가자' },
+        ]}
+      />
+    </section>
+  );
+};
--- marketplace_front/components/templates/index.ts
+++ marketplace_front/components/templates/index.ts
@@ -1,3 +1,4 @@
 export * from './home-token-list/home-token-list';
 export * from './layout/layout';
 export * from './create-token/create-token';
+export * from './home-transfer-history/home-transfer-history';
