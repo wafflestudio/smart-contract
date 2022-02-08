aa2544d4fb8543041cd43d654491a7ce9f76a515
guzus <50664161+guzus@users.noreply.github.com>
2022-02-08 22:21:05 +0900

    하는중..

--- marketplace_contract/contracts/WaffleExchange.sol
+++ marketplace_contract/contracts/WaffleExchange.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./util/LibAsset.sol";
+import "./util/LibOrder.sol";
 import "./interface/INftTransferProxy.sol";
 import "./interface/IERC20TransferProxy.sol";
+import "./interface/IWaffleExchange.sol";
 import "./WaffleExchangeProxyHandler.sol";
 
-contract WaffleExchange is WaffleExchangeProxyHandler {
-   /**
+contract WaffleExchange is WaffleExchangeProxyHandler, IWaffleExchange {
+    /**
      * @dev 거래 수수료
      */
     uint8 private exchangeFee;
@@ -15,9 +18,71 @@ contract WaffleExchange is WaffleExchangeProxyHandler {
     constructor(
         INftTransferProxy nftTransferProxy,
         IERC20TransferProxy erc20TransferProxy,
-        uint8 _exchangeFee 
+        uint8 _exchangeFee
     ) {
         initTransferProxy(nftTransferProxy, erc20TransferProxy);
         exchangeFee = _exchangeFee;
+    }
+
+    function registerOrder(
+        address maker,
+        LibAsset.Asset calldata makerAsset,
+        LibAsset.Asset calldata takerAsset
+    ) external returns (uint256) {
+        uint256 id = _getRandom(makerAsset.assetType.data);
+        orders.push(
+            LibOrder.Order(
+                maker,
+                makerAsset,
+                address(0),
+                takerAsset,
+                id,
+                LibOrder.OrderStatus.onSale
+            )
+        );
+        return id;
+    }
+
+    function matchOrder(
+        address taker,
+        uint256 id,
+        LibAsset.Asset calldata takerAsset
+    ) external returns (bool) {
+        LibOrder.Order order = orderOf(id);
+        _validateOrder(order);
+        _matchAndTransfer(order, taker, takerAsset);
+        return true;
+    }
+
+    function cancelOrder(address maker, uint256 id) external returns (bool) {
+        LibOrder.Order order = orderOf(id);
+        require(order.maker == maker == msg.sender());
+        require(order.status == LibOrder.OrderStatus.onSale);
+        order.status = LibOrder.OrderStatus.completed;
+        return true;
+    }
+
+    function _validateOrder(
+        LibOrder.Order memory order,
+        address taker,
+        LibAsset.Asset takerAsset
+    ) internal {
+        require(order.taker == 0x0);
+        require(order.status == LibOrder.OrderStatus.onSale);
+        require(order.takeAsset == takerAsset);
+        // require : does taker have enough balance?
+    }
+
+    function _matchAndTransfer(LibOrder.Order memory order) internal {
+        // maker -> taker (100% of order.makeAsset)
+        // proxies[].transfer()
+        // taker -> maker (96.7% of order.takeAsset)
+        // proxies[].transfer()
+        order.status = LibOrder.OrderStatus.completed;
+    }
+
+    function _getRandom(bytes memory data) internal view returns (uint256) {
+        return
+            uint256(keccak256(abi.encode(block.timestamp, msg.sender, data)));
     }
 }
---
+++ marketplace_contract/contracts/interface/IWaffleExchange.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../util/LibAsset.sol";
+import "../util/LibOrder.sol";
+
+abstract contract IWaffleExchange {
+    LibOrder.Order[] public orders;
+    mapping(uint256 => LibOrder.Order) public orderOf;
+
+    /**
+     * @dev NFT order 등록
+     * @return Order Id
+     */
+    function registerOrder(
+        address maker,
+        LibAsset.Asset calldata makeAsset,
+        LibAsset.Asset calldata takerAsset
+    ) external virtual returns (uint256);
+
+    // {
+    //     orders.push(Order {
+    //         maker,
+    //         makerAsset,
+    //         address(0),
+    //         takerAsset,
+    //         onSale
+    //     });
+    // }
+
+    /**
+     * @dev 등록된 NFT order 구매
+     * @param taker taker address
+     * @param id Order Id
+     * @param takerAsset taker asset
+     * @return 구매 성공 여부
+     */
+    function matchOrder(
+        address taker,
+        uint256 id,
+        LibAsset.Asset calldata takerAsset
+    ) external virtual returns (bool);
+
+    // {
+    //     Order order = orders[id]
+    //     _validateOrder(order, ...)
+    //     _matchAndTransfer(order)
+    // }
+
+    /**
+     * @dev 등록된 NFT 취소
+     * @return 취소 성공 여부
+     */
+    function cancelOrder(address maker, uint256 id)
+        external
+        virtual
+        returns (bool);
+
+    function _validateOrder(LibOrder.Order memory order) internal virtual;
+
+    function _matchAndTransfer(LibOrder.Order memory order) internal virtual;
+
+    // {
+    //     proxy를 통해 transfer
+    // }
+}
