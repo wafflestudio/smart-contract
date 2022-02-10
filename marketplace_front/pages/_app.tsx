c71ece7df247d3c941750a47a4e3016714930187
GoGiants1 <chooi221@snu.ac.kr>
2022-02-11 01:05:46 +0900

    지갑 연결

--- marketplace_front/pages/_app.tsx
+++ marketplace_front/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />{' '}
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
