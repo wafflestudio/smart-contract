60ecaeb9ddc2db5ab0a94bde26c216b8fab39f7a
j-min5u <ms.cheong@bucketplace.net>
2022-02-10 23:11:49 +0900

    feat implemented imaged token

--- waffle-721/hardhat.config.ts
+++ waffle-721/hardhat.config.ts
@@ -6,7 +6,6 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-import "@openzeppelin/hardhat-upgrades";
 
 dotenv.config();
 
