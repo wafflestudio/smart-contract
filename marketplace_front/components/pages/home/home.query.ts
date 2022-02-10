cffd411bffe393a6ee3cfb6b9652475a588cf68c
GoGiants1 <chooi221@snu.ac.kr>
2022-02-11 01:05:46 +0900

    지갑 연결

---
+++ marketplace_front/components/pages/home/home.query.ts
@@ -0,0 +1,21 @@
+import { useQuery } from 'react-query';
+
+import { provider } from '../../../library/ether';
+
+export const useAddress = () =>
+  useQuery<string>(
+    'address',
+    async () => {
+      if (!provider) throw Error;
+      const addressList: string[] = await provider.send('eth_requestAccounts', []);
+      if (addressList.length !== 1) throw Error;
+      return addressList[0];
+    },
+    {
+      onError: () => {
+        // toast.error('Metamask 에 계정이 한 개 올바르게 연결되어 있는지 확인해 주세요.');
+        alert('Metamask 에 계정이 한 개 올바르게 연결되어 있는지 확인해 주세요.');
+      },
+      enabled: !!provider,
+    }
+  );
