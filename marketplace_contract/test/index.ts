e271205f29867043d19d6302d2639cbece058ddb
JSKeum <jskeum2512@gmail.com>
2022-02-10 16:59:26 +0900

    add matchOrder test

--- marketplace_contract/test/index.ts
+++ marketplace_contract/test/index.ts
@@ -10,25 +10,32 @@ describe("WaffleExchange", function () {
   let erc20Proxy: Contract;
   let exchangeAdmin: SignerWithAddress;
   let orderMaker: SignerWithAddress;
-  let ordertkaer: SignerWithAddress;
+  let ordertaker: SignerWithAddress;
 
   let fee: number;
 
   beforeEach(async () => {
-    nftProxy = await (
-      await ethers.getContractFactory("NftTransferProxy")
-    ).deploy();
+    [exchangeAdmin, orderMaker, ordertaker] = await ethers.getSigners();
 
-    erc20Proxy = await (
-      await ethers.getContractFactory("ERC20TransferProxy")
-    ).deploy();
+    nftProxy = await (await ethers.getContractFactory("NftTransferProxy"))
+      .connect(exchangeAdmin)
+      .deploy();
+
+    erc20Proxy = await (await ethers.getContractFactory("ERC20TransferProxy"))
+      .connect(exchangeAdmin)
+      .deploy();
 
     fee = 30;
-    waffleExchange = await (
-      await ethers.getContractFactory("WaffleExchange")
-    ).deploy(nftProxy.address, erc20Proxy.address, fee);
 
-    [exchangeAdmin, orderMaker, ordertkaer] = await ethers.getSigners();
+    waffleExchange = await (await ethers.getContractFactory("WaffleExchange"))
+      .connect(exchangeAdmin)
+      .deploy(nftProxy.address, erc20Proxy.address, fee);
+
+    await nftProxy.connect(exchangeAdmin).__TransferProxy_init();
+    await erc20Proxy.connect(exchangeAdmin).__ERC20TransferProxy_init();
+
+    await nftProxy.connect(exchangeAdmin).addOperator(waffleExchange.address);
+    await erc20Proxy.connect(exchangeAdmin).addOperator(waffleExchange.address);
   });
   it("Should return exchange fee dominator once it's deployed", async function () {
     const exchangeFee = await waffleExchange.getExchangeFee();
@@ -77,6 +84,32 @@ describe("WaffleExchange", function () {
 
     expect(makeAsset).to.eql(registeredOrderMakerAsset);
     expect(takeAsset).to.eql(registeredOrderTakerAsset);
+  });
+  it("Should match order with correct maker asset and taker asset", async function () {
+    const testErc721 = await (
+      await ethers.getContractFactory("TestERC721")
+    ).deploy("testName", "testSymbol");
+
+    testErc721.mint(orderMaker.address, 7, "");
+    testErc721.connect(orderMaker).approve(nftProxy.address, 7);
+
+    const testErc20 = await (
+      await ethers.getContractFactory("TestERC20")
+    ).deploy("waffleCoin", "waffle");
+
+    testErc20.mint(ordertaker.address, 1000);
+    testErc20.connect(ordertaker).approve(erc20Proxy.address, 10);
+
+    const makeAsset = asset(ERC721, encodeAbi(testErc721.address, 7), 1);
+    const takeAsset = asset(ERC20, encodeAbi(testErc20.address), 10);
+
+    await waffleExchange.registerOrder(
+      orderMaker.address,
+      makeAsset,
+      takeAsset
+    );
+
+    await waffleExchange.matchOrder(ordertaker.address, 1, takeAsset);
   });
   it("Should return proxy addresses once it's initilaized or changed", async function () {
     // TODO : proxy 정보를 불러오는 함수를 추가해야 합니다
