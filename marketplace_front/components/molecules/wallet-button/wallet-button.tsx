ec1ad44c2fd5889d1ae7c10db32dc31d99c643eb
woohm402 <woohm404@gmail.com>
2022-02-11 20:42:48 +0900

    address 정보 context 로 이동

--- marketplace_front/components/molecules/wallet-button/wallet-button.tsx
+++ marketplace_front/components/molecules/wallet-button/wallet-button.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-
 import classNames from 'classnames';
 import toast from 'react-hot-toast';
 
+import { useMetamaskContext } from '../../../contexts/metamaskContext';
 import { connectWallet } from '../../../library/ether';
 
 import styles from './wallet-button.module.scss';
@@ -12,10 +11,10 @@ interface Props {
 }
 
 export const WalletButton = ({ className }: Props) => {
-  const [address, setAddress] = useState<string | null>(null);
+  const { address, setAddress, clearAddress } = useMetamaskContext();
   const onClickWallet = async () => {
     if (address) {
-      setAddress(null);
+      clearAddress();
       toast.success(`metamask 계정 연결이 해제되었습니다.`);
       return;
     }
@@ -27,14 +26,15 @@ export const WalletButton = ({ className }: Props) => {
       toast.success(`metamask 계정이 연결되었습니다.`);
     } catch (err) {
       toast.error('metamask 에 연결하던 중 오류가 발생했습니다.');
-      setAddress(null);
+      clearAddress();
     }
   };
 
   return (
     <button
       className={classNames(className, styles.walletButton, address ? styles.connected : styles.disconnected)}
-      onClick={() => onClickWallet()}
+      onClick={onClickWallet}
+      title={address ?? undefined}
     >
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/32px-MetaMask_Fox.svg.png"
