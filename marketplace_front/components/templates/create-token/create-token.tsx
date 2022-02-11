f996c1f05594dc7f2799e2910030bcacb50f5083
woohm402 <woohm404@gmail.com>
2022-02-11 23:29:28 +0900

    토큰 생성 모달 구현

--- marketplace_front/components/templates/create-token/create-token.tsx
+++ marketplace_front/components/templates/create-token/create-token.tsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
+
+import Modal from 'react-modal';
+
 import { Typography } from '../../atoms';
 import { MintButton } from '../../molecules';
+import { CreateTokenModalContent } from '../../organisms/create-token-modal-content/create-token-modal-content';
 
 import styles from './create-token.module.scss';
 
 export const CreateToken = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
-    <section className={styles.wrapper}>
-      <Typography as="h3">토큰 만들기</Typography>
-      <Typography className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-        aliqua.
-      </Typography>
-      <MintButton className={styles.mintbutton} />
-    </section>
+    <>
+      <section className={styles.wrapper}>
+        <Typography as="h3">토큰 만들기</Typography>
+        <Typography className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua.
+        </Typography>
+        <MintButton className={styles.mintbutton} onClick={() => setModalOpen(true)} />
+      </section>
+      <Modal
+        style={{ overlay: { zIndex: 9 } }}
+        className={styles.modal}
+        isOpen={isModalOpen}
+        onRequestClose={() => setModalOpen(false)}
+      >
+        <CreateTokenModalContent />
+      </Modal>
+    </>
   );
 };
