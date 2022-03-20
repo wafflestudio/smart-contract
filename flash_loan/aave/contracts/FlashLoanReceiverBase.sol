e972858cbac75f70c484b69c9e928924378b2f85
JSKeum <jskeum2512@gmail.com>
2022-03-20 20:45:59 +0900

    add: add aave flashloan

---
+++ flash_loan/aave/contracts/FlashLoanReceiverBase.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./IFlashLoanReceiver.sol";
+import "./ILendingPoolAddressesProvider.sol";
+import "./Withdrawable.sol";
+
+abstract contract FlashLoanReceiverBaseV1 is IFlashLoanReceiverV1, Withdrawable {
+
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    address constant ethAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    ILendingPoolAddressesProviderV1 public addressesProvider;
+
+    constructor(address _addressProvider) {
+        addressesProvider = ILendingPoolAddressesProviderV1(_addressProvider);
+    }
+
+    receive() payable external {}
+
+    function transferFundsBackToPoolInternal(address _reserve, uint256 _amount) internal {
+        address payable core = addressesProvider.getLendingPoolCore();
+        transferInternal(core, _reserve, _amount);
+    }
+
+    function transferInternal(address payable _destination, address _reserve, uint256 _amount) internal {
+        if(_reserve == ethAddress) {
+            (bool success, ) = _destination.call{value: _amount}("");
+            require(success == true, "Couldn't transfer ETH");
+            return;
+        }
+        IERC20(_reserve).safeTransfer(_destination, _amount);
+    }
+
+    function getBalanceInternal(address _target, address _reserve) internal view returns(uint256) {
+        if(_reserve == ethAddress) {
+            return _target.balance;
+        }
+        return IERC20(_reserve).balanceOf(_target);
+    }
+}
